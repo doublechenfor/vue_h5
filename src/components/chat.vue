146a2ceabce4a39002fd20f573c18ed70fc8e67a
chenshuang <chenshuang_soleage@163.com>
2020-04-05 22:20:44 +0800

    feat:socity-社交圈

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -45,16 +45,33 @@
                                 </ul>
                             </transition>
                         </div>
-                    </div>
-                    
+                    </div>                    
                 </mt-tab-container-item>
                 <mt-tab-container-item id="3">
-                    <mt-cell v-for="n in 6" :title="'选项 ' + n" />
+                    <div class="socity-blog">
+                        <ul>
+                            <li v-for="(item,index) in socityLists">
+                                <div>
+                                    <h4>{{item.name}}</h4>
+                                    <p>{{item.text.length>=60?item.text.substring(0,70)+'...':item.text}}</p>
+                                    <img :src="item.src"/>
+                                    <div class="show-attitude">
+                                        <svgIcon iconClass="thumb-up"/>
+                                        {{item.like}}
+                                    </div>
+                                    <div class="socity-comments" v-for="i in item.comments">
+                                        <label>{{i.alias}}:</label>
+                                        <p>{{i.comment}}</p>
+                                    </div>
+                                </div>
+                                <input placeholder="评论" />
+                            </li>
+                        </ul>
+                    </div>
                 </mt-tab-container-item>
         </mt-tab-container>
         </mt-loadmore>
         <!-- tab-container -->
-
     </div>
 </template>
 <script>
@@ -63,7 +80,7 @@ export default {
     name:'chat',
     data(){
         return{
-            selected:'1',
+            selected:'3',
             chatLists:[
                 {name:'张三', msg:'明天会不会放假?',icon:require('../assets/images/cat.jpg'),timestamp:'13:40',unreadNum:20},
                 {name:'史密斯', msg:'hello?',icon:require('../assets/images/cat.jpg'),timestamp:'13:40',unreadNum:20},
@@ -98,8 +115,34 @@ export default {
                     {icon:require('../assets/images/cat.jpg'),device:'4G在线',alias:'春秋游'},
                     {icon:require('../assets/images/cat.jpg'),device:'4G在线',alias:'王五'},
                     {icon:require('../assets/images/cat.jpg'),device:'4G在线',alias:'罗大佑'},
+                ]},                
+            ],
+            socityLists:[
+                {name:'罗永浩',text:'罗永浩带货直播首秀交出的成绩单看起来不错。尽管很多人觉得直播的节奏过慢，且东西并不便宜，但罗永浩用7万多支中性笔几分钟售罄的“带货能力”表明，自己在互联网界仍旧具有举足轻重的影响力。为了请来罗永浩直播，抖音花了6000万。这6000万花得值吗？从罗永浩直播首秀带来的话题关注度看，这钱是花得值。对抖音而言，它需要开拓自己的直播业务和带货业务，增强自己的变现能力。如果从自身已有的中小主播中培养出一个李佳琪、薇娅级别的网红主播，需耗时很长且未必有成效。而请来本身具有巨大流量的罗永浩，能很快打响自身业务的知名度。很多人正是从昨夜罗永浩的直播中，第一次得知抖音还能直播卖货，也是第一次在抖音上买东西。同时，抖音也以此向品牌合作方宣示，自己拥有强大的卖货能力。这也会促成更多商家进驻抖音。',src:require('../assets/images/cat.jpg'),
+                like:21, comments:[
+                    {alias:'古道流水人家',comment:'罗永浩来嚯嚯直播带货咯~~'},
+                    {alias:'飞鸟爱上鱼',comment:'4.1日的tik tok 直播,效果不行,不行!!'},
                 ]},
-                
+                {name:'罗永浩',text:'罗永浩带货直播首秀交出的成绩单看起来不错。尽管很多人觉得直播的节奏过慢，且东西并不便宜，但罗永浩用7万多支中性笔几分钟售罄的“带货能力”表明，自己在互联网界仍旧具有举足轻重的影响力。为了请来罗永浩直播，抖音花了6000万。这6000万花得值吗？从罗永浩直播首秀带来的话题关注度看，这钱是花得值。对抖音而言，它需要开拓自己的直播业务和带货业务，增强自己的变现能力。如果从自身已有的中小主播中培养出一个李佳琪、薇娅级别的网红主播，需耗时很长且未必有成效。而请来本身具有巨大流量的罗永浩，能很快打响自身业务的知名度。很多人正是从昨夜罗永浩的直播中，第一次得知抖音还能直播卖货，也是第一次在抖音上买东西。同时，抖音也以此向品牌合作方宣示，自己拥有强大的卖货能力。这也会促成更多商家进驻抖音。',src:require('../assets/images/cat.jpg'),like:21, comments:[
+                    {alias:'古道流水人家',comment:'罗永浩来嚯嚯直播带货咯~~'},
+                    {alias:'飞鸟爱上鱼',comment:'4.1日的tik tok 直播,效果不行,不行!!'},
+                ]},
+                {name:'罗永浩',text:'罗永浩带货直播首秀交出的成绩单看起来不错。尽管很多人觉得直播的节奏过慢，且东西并不便宜，但罗永浩用7万多支中性笔几分钟售罄的“带货能力”表明，自己在互联网界仍旧具有举足轻重的影响力。为了请来罗永浩直播，抖音花了6000万。这6000万花得值吗？从罗永浩直播首秀带来的话题关注度看，这钱是花得值。对抖音而言，它需要开拓自己的直播业务和带货业务，增强自己的变现能力。如果从自身已有的中小主播中培养出一个李佳琪、薇娅级别的网红主播，需耗时很长且未必有成效。而请来本身具有巨大流量的罗永浩，能很快打响自身业务的知名度。很多人正是从昨夜罗永浩的直播中，第一次得知抖音还能直播卖货，也是第一次在抖音上买东西。同时，抖音也以此向品牌合作方宣示，自己拥有强大的卖货能力。这也会促成更多商家进驻抖音。',src:require('../assets/images/cat.jpg'),like:21, comments:[
+                    {alias:'古道流水人家',comment:'罗永浩来嚯嚯直播带货咯~~'},
+                    {alias:'飞鸟爱上鱼',comment:'4.1日的tik tok 直播,效果不行,不行!!'},
+                ]},
+                {name:'罗永浩',text:'罗永浩带货直播首秀交出的成绩单看起来不错。尽管很多人觉得直播的节奏过慢，且东西并不便宜，但罗永浩用7万多支中性笔几分钟售罄的“带货能力”表明，自己在互联网界仍旧具有举足轻重的影响力。为了请来罗永浩直播，抖音花了6000万。这6000万花得值吗？从罗永浩直播首秀带来的话题关注度看，这钱是花得值。对抖音而言，它需要开拓自己的直播业务和带货业务，增强自己的变现能力。如果从自身已有的中小主播中培养出一个李佳琪、薇娅级别的网红主播，需耗时很长且未必有成效。而请来本身具有巨大流量的罗永浩，能很快打响自身业务的知名度。很多人正是从昨夜罗永浩的直播中，第一次得知抖音还能直播卖货，也是第一次在抖音上买东西。同时，抖音也以此向品牌合作方宣示，自己拥有强大的卖货能力。这也会促成更多商家进驻抖音。',src:require('../assets/images/cat.jpg'),like:21, comments:[
+                    {alias:'古道流水人家',comment:'罗永浩来嚯嚯直播带货咯~~'},
+                    {alias:'飞鸟爱上鱼',comment:'4.1日的tik tok 直播,效果不行,不行!!'},
+                ]},
+                {name:'罗永浩',text:'罗永浩带货直播首秀交出的成绩单看起来不错。尽管很多人觉得直播的节奏过慢，且东西并不便宜，但罗永浩用7万多支中性笔几分钟售罄的“带货能力”表明，自己在互联网界仍旧具有举足轻重的影响力。为了请来罗永浩直播，抖音花了6000万。这6000万花得值吗？从罗永浩直播首秀带来的话题关注度看，这钱是花得值。对抖音而言，它需要开拓自己的直播业务和带货业务，增强自己的变现能力。如果从自身已有的中小主播中培养出一个李佳琪、薇娅级别的网红主播，需耗时很长且未必有成效。而请来本身具有巨大流量的罗永浩，能很快打响自身业务的知名度。很多人正是从昨夜罗永浩的直播中，第一次得知抖音还能直播卖货，也是第一次在抖音上买东西。同时，抖音也以此向品牌合作方宣示，自己拥有强大的卖货能力。这也会促成更多商家进驻抖音。',src:require('../assets/images/cat.jpg'),like:21, comments:[
+                    {alias:'古道流水人家',comment:'罗永浩来嚯嚯直播带货咯~~'},
+                    {alias:'飞鸟爱上鱼',comment:'4.1日的tik tok 直播,效果不行,不行!!'},
+                ]},
+                {name:'罗永浩',text:'罗永浩带货直播首秀交出的成绩单看起来不错。尽管很多人觉得直播的节奏过慢，且东西并不便宜，但罗永浩用7万多支中性笔几分钟售罄的“带货能力”表明，自己在互联网界仍旧具有举足轻重的影响力。为了请来罗永浩直播，抖音花了6000万。这6000万花得值吗？从罗永浩直播首秀带来的话题关注度看，这钱是花得值。对抖音而言，它需要开拓自己的直播业务和带货业务，增强自己的变现能力。如果从自身已有的中小主播中培养出一个李佳琪、薇娅级别的网红主播，需耗时很长且未必有成效。而请来本身具有巨大流量的罗永浩，能很快打响自身业务的知名度。很多人正是从昨夜罗永浩的直播中，第一次得知抖音还能直播卖货，也是第一次在抖音上买东西。同时，抖音也以此向品牌合作方宣示，自己拥有强大的卖货能力。这也会促成更多商家进驻抖音。',src:require('../assets/images/cat.jpg'),like:21, comments:[
+                    {alias:'古道流水人家',comment:'罗永浩来嚯嚯直播带货咯~~'},
+                    {alias:'飞鸟爱上鱼',comment:'4.1日的tik tok 直播,效果不行,不行!!'},
+                ]}
             ]
         }
     },
@@ -119,6 +162,7 @@ export default {
 .chat-wrapper{
     width: 100vw;
     height: 100vh;
+    background: #F4F4F4;
     .chat-list{
         width: calc(100% - 2rem);
         margin-bottom: 55px;
@@ -164,6 +208,7 @@ export default {
         width:calc(100% - 2rem);
         margin-bottom: 55px;
         padding: 0.5rem 1rem;
+        background: #F4F4F4;
         .chat-group-li{
             width: 100%;
             &>:first-child{
@@ -204,6 +249,60 @@ export default {
                         display: block;                     
                         font-size: 0.7rem;
                         color: #B9B9B9;
+                    }
+                }
+            }
+        }
+    }
+    // 朋友圈
+    .socity-blog{
+        width: 100vw;
+        padding: 0.5rem 0;
+        padding-bottom: 55px;
+        background: #F4F4F4;
+        ul{
+            width: 100%;
+            li{ 
+                padding: 0 0.5rem;
+                background: #fff;
+                margin-bottom: 1rem;
+                & >:first-child{
+                    h4{
+                        padding: 0.5rem;
+                    }
+                    p{
+                        line-height: 1.5rem;
+                    }
+                    img{
+                        width: 8rem;
+                        height: 6rem;
+                    }
+                }
+                input{
+                    height: 2rem;
+                    width: calc(100% - 1rem);
+                    background: #F4F4F4;
+                    margin: 0.5rem 0;
+                    border-radius: 0.3rem;
+                    padding: 0.1rem 0.5rem;
+                    &:focus{
+                        outline: none;
+                    }
+                }
+                .show-attitude{
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: flex-end;
+                }
+                .socity-comments{
+                    display: flex;
+                    flex-direction: row;
+                    line-height: 1.5rem;
+                    label{
+                        color: #26A2FF;
+                    }
+                    p{
+                        margin-left: 0.5rem;
                     }
                 }
             }
